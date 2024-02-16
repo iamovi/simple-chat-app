599e7330620fede18da59e3bed35c275ec9d8207
Maruf OVi <137372623+iamovi@users.noreply.github.com>
2024-02-16 21:57:18 +0600

    Update app.js to show localhost link

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
